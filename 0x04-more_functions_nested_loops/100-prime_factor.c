4875d86b2a6c754e6858eb9cdbd45dd2dff60eff
abdo-elshaikh <abdo202224@gmail.com>
2023-06-22 11:38:59 -0700

    primary factory

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description:finds and prints the largest prime factor of the number
+ * Return: void
+ */
+int main(void)
+{
+	long int num = 612852475143;
+	long int i = 2;
+
+	while(i < num)
+	{
+		if (num % i == 0)
+			num = num / i;
+		i++;
+	}
+	printf("%ld\n", i);
+	return (0);
+}
