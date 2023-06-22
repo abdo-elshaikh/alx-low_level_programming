8a0b759685d3b4ed44840ffef05132b7a1efe0c2
abdo-elshaikh <abdo202224@gmail.com>
2023-06-22 12:21:10 -0700

    print num

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,23 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	unsigned int n1 = 0;
+
+	if  (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		n = -n;
 	}
-	if (n / 10 != 0)
+
+	else
 	{
-		print_number(n / 10);
+		n1 = n;
 	}
-	_putchar('0' + (n % 10));
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
