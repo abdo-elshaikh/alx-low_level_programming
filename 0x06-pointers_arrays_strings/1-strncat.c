eac34dccbdda877726e2dafc004b7006ff52dbc1
abdo-elshaikh <132365018+abdo-elshaikh@users.noreply.github.com>
2023-06-29 19:04:52 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,15 +8,12 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, x = 0;
+	int dest_len, i;
 
-	while (dest[x])
-		x++;
-	for (i = 0; i < n && src[i]; i++)
-	{
-		dest[x + i] = src[i];
-		x++;
-	}
-	dest[x + i] = '\0';
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	/*should end with a end of string char*/
+	dest[dest_len + i] = '\0';
 	return (dest);
 }
