7036013d9cff848b3549938bf67cba1f80191d72
abdo-elshaikh <132365018+abdo-elshaikh@users.noreply.github.com>
2023-06-29 19:39:44 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _strcmp - copire betwine to string
- * @s1: first string
- * @s2: second string
- * Retuen: 0 if matching , ns1 - ns2 if not matching
- */
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 if matching, and ns1 - ns2 if not matching
+*/
 int _strcmp(char *s1, char *s2)
 {
 	int i;
