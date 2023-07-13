4ecf5048002975eab007ab1d8e821dbac2c1f0dc
abdo-elshaikh <abdo202224@gmail.com>
2023-07-13 07:19:49 -0700

    update str concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,63 +1,60 @@
 #include "main.h"
 #include <stdlib.h>
+unsigned int _strlen(char *s);
+
 /**
- * _strlen - sttring length
- * @s: get string
- * Return: len
+ * string_nconcat - concatenates two strings,
+ * @s1: first string .
+ * @s2: second string.
+ * @n: numbers of chars to concatinate from the second string.
+ * Return: pointer to the new string.
+ **/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0, j, s1_len, s2_len;
+	char *aux;
+
+	s1_len = _strlen(s1);
+	s2_len = _strlen(s2);
+
+	if (n < s2_len + 1)
+		aux = malloc(sizeof(char) * (s1_len + n + 1));
+	else
+		aux = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (aux == NULL)
+		return (NULL);
+
+	if (s1 != NULL)
+		for (i = 0; s1[i] != '\0'; i++)
+			aux[i] = s1[i];
+	if (s2 != NULL)
+	{
+		if (n < s2_len)
+			for (j = 0; j < n; j++, i++)
+				aux[i] = s2[j];
+		else
+			for (j = 0; s2[j] != '\0'; j++, i++)
+				aux[i] = s2[j];
+	}
+	aux[i] = '\0';
+	return (aux);
+}
+/**
+ * _strlen - returns the length of a string.
+ * @s: string.
+ * Return: Length of @s.
  */
 unsigned int _strlen(char *s)
 {
 	unsigned int i = 0;
+	unsigned int cont = 0;
 
+	if (s == NULL)
+		return (0);
 	while (s[i] != '\0')
 	{
+		cont++;
 		i++;
 	}
-	return (i);
-}
-/**
- * string_nconcat - conact strings
- * @s1: first string
- * @s2: second string
- * @n: count intager
- * Return: pointer or NULL
- */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	char *ptr;
-	unsigned int len1 = 0, len2 = 0;
-	unsigned int i, j;
-
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-
-	ptr = malloc(((len1 + len2) * sizeof(char)) + 1);
-	if (ptr == NULL)
-		return (NULL);
-
-	if (s1 != NULL)
-	{
-		if (n >= len2)
-		{
-			for (i = 0; s1[i] != '\0'; i++)
-				ptr[i] = s1[i];
-			if (s2 != NULL)
-			{
-				for (j = 0; s2[i] != '\0'; j++, i++)
-					ptr[i] = s2[j];
-			}
-		}
-		else
-		{
-			for (i = 0; s1[i] != '\0'; i++)
-				ptr[i] = s1[i];
-			if (s2 != NULL)
-			{
-				for (j = 0; j < n; j++, i++)
-					ptr[i] = s2[j];
-			}
-		}
-	}
-	ptr[i] = '\0';
-	return (ptr);
+	return (cont);
 }
