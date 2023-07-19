395c6d07f0f7cfc26bc1781ae123063f8433d74a
abdo-elshaikh <132365018+abdo-elshaikh@users.noreply.github.com>
2023-07-19 20:59:10 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,30 +1,29 @@
 #include "3-calc.h"
+
 /**
- * main - main block
- * @argc: arg count
- * @argv: arg value
- * Return: 0
+ * main - performs simple math operations.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: return
  */
 int main(int argc, char *argv[])
 {
-	int a, b, result;
-	char *s;
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	s = argv[2];
+	int a, b;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (get_op_func(s) == NULL)
+	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	result = get_op_func(s)(a, b);
-	printf("%d\n", result);
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%d\n", get_op_func(argv[2])(a, b));
+
 	return (0);
 }
