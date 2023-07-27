290619d7dc8e56955f9f6b22f4000924e950a34a
abdo-elshaikh <abdo202224@gmail.com>
2023-07-27 10:05:29 -0700

    update task1 advanced

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-void print_before_main() __attribute__((constructor));
 /**
  * print_befor_main - function
  * desciription: print function befor main
  * Return: void
  */
-void print_before_main()
+void __attribute__((constructor))print_before_main()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
